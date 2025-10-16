1024051e39fcfe9877f9fba943b5599d3db6882f
dylantylerbeach <dylantylerbeach@gmail.com>
2025-10-16 03:18:51 -0700

    navigation, home page and favorite page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Route,Routes} from "react-router-dom"
+import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
+
+/*function App() {
+    return (
+        <>
+            <Text display = "whats up"/>
+            <Text display = "hello" />
+        </>
+    );
+}
+
+function Text({display}) {
+    return (
+        <div>
+            <p>{display}</p>
+        </div>
+    );
+}*/
 
 function App() {
-  const [count, setCount] = useState(0)
+    return(
+        <>
+            <div>
+                <NavBar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                </Routes>
+            </main>
+            </div>
+        </>
+
+    );
 }
 
 export default App
